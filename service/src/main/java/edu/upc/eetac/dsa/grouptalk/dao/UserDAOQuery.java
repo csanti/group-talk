107cfce660e457a8a6d853c4df541c33f5f990b3
Carlos Santiago <carlos.san.rol@gmail.com>
2016-04-02 23:39:36 +0200

    Primer build funcionando con UserResource añadido

--- service/src/main/java/edu/upc/eetac/dsa/grouptalk/dao/UserDAOQuery.java
+++ service/src/main/java/edu/upc/eetac/dsa/grouptalk/dao/UserDAOQuery.java
@@ -7,7 +7,7 @@ public interface UserDAOQuery {
     public final static String UUID = "select REPLACE(UUID(),'-','')";
     public final static String CREATE_USER = "insert into users (id, loginid, password, email, fullname) values (UNHEX(?), ?, UNHEX(MD5(?)), ?, ?);";
     public final static String UPDATE_USER = "update users set email=?, fullname=? where id=unhex(?)";
-    public final static String ASSIGN_ROLE_REGISTERED = "insert into user_roles (userid, role) values (UNHEX(?), ?)";
+    public final static String ASSIGN_ROLE_REGISTERED = "insert into user_roles (userid, role) values (UNHEX(?), 'registered')";
     public final static String GET_USER_BY_ID = "select hex(u.id) as id, u.loginid, u.email, u.fullname from users u where id=unhex(?)";
     public final static String GET_USER_BY_USERNAME = "select hex(u.id) as id, u.loginid, u.email, u.fullname from users u where u.loginid=?";
     public final static String DELETE_USER = "delete from users where id=unhex(?)";
---
+++ service/src/main/java/edu/upc/eetac/dsa/grouptalk/entity/GroupTalkMediaType.java
@@ -0,0 +1,10 @@
+package edu.upc.eetac.dsa.grouptalk.entity;
+
+/**
+ * Created by carlos on 2/04/16.
+ */
+public interface GroupTalkMediaType {
+    public final static String GROUPTALK_AUTH_TOKEN = "application/vnd.dsa.grouptalk.auth-token+json";
+    public final static String GROUPTALK_USER = "application/vnd.dsa.grouptalk.user+json";
+    public final static String GROUPTALK_ROOT = "application/vnd.dsa.grouptalk.root+json";
+}
